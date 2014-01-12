2badd4e0bbb8c474d654c9d2857980054ab55cb3
Nicole Paz <nicole@unknowne4ce8f7aaf5f.gateway.2wire.net>
2014-01-12 13:18:10 -0800

    Added link to show view for flashcards.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -8,5 +8,11 @@ class SubjectsController < ApplicationController
   def find_all subject
     Flashcard.where(subject_id: subject.id)
   end
+
+  def find_flash card
+    @card = card
+  end
+
   helper_method :find_all
+  helper_method :find_flash
 end
