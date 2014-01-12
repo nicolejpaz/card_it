d5ec2a4ebca04db0e925a4f0f9e73b2dfb6aa67f
Nicole Paz <nicole@unknowne4ce8f7aaf5f.gateway.2wire.net>
2014-01-12 13:10:35 -0800

    Added helper method to try to keep code DRY.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,12 @@
 class SubjectsController < ApplicationController
   def index
-    @cards = Flashcard.all
+    @subjects = Subject.all
   end
+
+  private 
+
+  def find_all subject
+    Flashcard.where(subject_id: subject.id)
+  end
+  helper_method :find_all
 end
