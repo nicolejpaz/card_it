a5b15d2c3c0e097d1086d6be641727509960dba9
Nicole Paz <nicole@unknowne4ce8f7aaf5f.gateway.2wire.net>
2014-01-12 15:22:09 -0800

    Changed views so that first question shows up on page.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -3,6 +3,6 @@ class DecksController < ApplicationController
     @deck_id = params['id']
     @deck = Deck.find(@deck_id)
 
-    @card = Card.where(deck_id: @deck_id).first
+    @card = Card.where(deck_id: @deck_id)
   end
 end
