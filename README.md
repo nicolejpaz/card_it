5c9afdf259d3fccd1aa9a543b5beaa7907451205
Nicole Paz <nicole@unknowne4ce8f7aaf5f.gateway.2wire.net>
2014-01-11 20:20:40 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Card It!
+
+Need to memorize something fast? Don't want to spend the time or money on flashcards? Then this is for you! Select a subject and start learning right away!
+
+V0:
+
+* NATO phonetic alphabet
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CardIt::Application.config.secret_key_base = '6aef3b8915e1d525414d9c8d60b879183a0cdecfbe8cd573a94bcd767508142918968a5788cfd1e418b7c54a8159c4845eabec6147d1933dcaadcf13509a69ec'
