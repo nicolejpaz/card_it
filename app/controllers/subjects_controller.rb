9365e5a2e7fdbe71edd767c47dbc6571a981a5bb
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2014-01-13 19:18:18 -0800

    Removed helper methods.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,17 +2,4 @@ class SubjectsController < ApplicationController
   def index
     @subjects = Subject.all
   end
-
-  private 
-
-  def find_all subject
-    Deck.where(subject_id: subject.id)
-  end
-
-  def find_flash card
-    @card = card
-  end
-
-  helper_method :find_all
-  helper_method :find_flash
 end
